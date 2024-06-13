bdb70d632d0779aec5f33cb8786a10203094538b
Runsumin <tnalsdl9636@gmail.com>
2024-06-13 23:22:12 +0900

    스크립트 정리, 카메라 셋팅 추가
    기존 스크립트 백업 파일로 이동, 카메라 셋팅 스크립트 추가

---
+++ Assets/Scripts/02. InGame/00. Player/DCL_PlayerBase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HSM.Game
+{
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //
+    // DCL_PlayerBase
+    // 플레이어 베이스 클래스
+    //
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+    public class DCL_PlayerBase : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
