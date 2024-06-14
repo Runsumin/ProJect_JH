825db44d02d4ecc414864a9ddd0d635b5878827a
Runsumin <tnalsdl9636@gmail.com>
2024-06-14 13:31:25 +0900

    프레임워크 작업중

--- Assets/Scripts/02. InGame/00. Player/DCL_PlayerBase.cs
+++ Assets/Scripts/02. InGame/00. Player/DCL_PlayerBase.cs
@@ -7,24 +7,24 @@ namespace HSM.Game
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     //
     // DCL_PlayerBase
-    // 플레이어 베이스 클래스
+    // 플레이어 베이스 클래스 , 플레이어의 상태를 관리한다
     //
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-    public class DCL_PlayerBase : MonoBehaviour
+    public class DCL_PlayerBase : ObjectBase
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        //// Start is called before the first frame update
+        //void Start()
+        //{
 
-        }
+        //}
 
-        // Update is called once per frame
-        void Update()
-        {
+        //// Update is called once per frame
+        //void Update()
+        //{
 
-        }
+        //}
     }
 
 }
--- Assets/Scripts/02. InGame/00. Player/DCL_PlayerInput.cs
+++ Assets/Scripts/02. InGame/00. Player/DCL_PlayerInput.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DCL_PlayerInput : MonoBehaviour
+namespace HSM.Game
 {
-    // Start is called before the first frame update
-    void Start()
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //
+    // DCL_PlayerInput
+    // 플레이어 인풋 클래스. 플레이어의 입력값 관리 클래스
+    //
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    public class DCL_PlayerInput : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float moveZ = 0f;
+            float moveX = 0f;
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveX += 1f;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveX -= 1f;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveZ -= 1f;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveZ += 1f;
+            }
+
+            transform.Translate(new Vector3(moveX, 0f, moveZ) * 0.1f);
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
