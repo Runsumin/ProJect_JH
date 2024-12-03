42dd48b161893835ac8b833ae0ed9c5bc650794b
Runsumin <72703196+Runsumin@users.noreply.github.com>
2024-12-03 22:05:26 +0900

    테스트 진행중

---
+++ Assets/Scripts/02. InGame/06. Monster/BTcs/AttackStart.cs
@@ -0,0 +1,34 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "AttackStart", story: "[Attack] Start in [Animator]", category: "Action", id: "1ee51aa4765c292f43cff63daf3eccb2")]
+public partial class AttackStartAction : Action
+{
+    [SerializeReference] public BlackboardVariable<string> Attack;
+    [SerializeReference] public BlackboardVariable<Animator> Animator;
+
+    protected override Status OnStart()
+    {
+        Animator.Value.SetTrigger(Attack);
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        if (Animator.Value.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+            return Status.Running;
+        else
+        {
+            return Status.Success;
+        }
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
